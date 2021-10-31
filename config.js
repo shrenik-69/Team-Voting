a78f3e6f35a66068f0b3955d5131d79b0a929b3d
shrenik <vsrgaming67@gmail.com>
2021-10-31 17:34:08 +0530

    edited config

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+        apiKey: "AIzaSyDXFAR1AYNNNQaxbHX_pAZrJZMapLzbM5E",
+        authDomain: "voting-2583a.firebaseapp.com",
+        projectId: "voting-2583a",
+        storageBucket: "voting-2583a.appspot.com",
+        messagingSenderId: "488088520078",
+        appId: "1:488088520078:web:ef4c68086f4d6890ccd0df"
 };
-// Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
